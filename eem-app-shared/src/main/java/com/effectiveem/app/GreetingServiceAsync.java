061f2aea3fb4051cb99a167dd6b2250a9a29130c
simeshev <imeshev@gmail.com>
2022-10-16 12:00:12 -0700

    Initial commit

---
+++ eem-app-shared/src/main/java/com/effectiveem/app/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package com.effectiveem.app;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<GreetingResponse> callback);
+}
