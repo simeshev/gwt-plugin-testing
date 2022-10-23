6031ece371dc5136a653924bc48480eb6e613ff2
simeshev <imeshev@gmail.com>
2022-10-23 12:10:21 -0700

    Added a note on a error caused by an unrelated fully quality module path.

--- eem-app-client/src/test/java/com/effectiveem/app/table/TableRowVOTest.java
+++ eem-app-client/src/test/java/com/effectiveem/app/table/TableRowVOTest.java
@@ -23,10 +23,16 @@ public class TableRowVOTest extends GWTTestCase {
    @Override
    public String getModuleName() {
 
-      // NOTE: simeshev - 2023-10-22 - It's critical that this literal is set to the name
+      // NOTE: simeshev - 2022-10-22 - It's critical that this literal is set to the name
       // as defined in the configuration of net.ltgt.gwt.maven's gwt-maven-plugin
       // <moduleName>...</moduleName> in this client's pom.xml. Otherwise, if using the shor modeul name such as "eemapp",
       // there will be errors such as "Unable to find 'eemapp.gwt.xml' on your classpath; could be a typo, or maybe you forgot to include a classpath entry for source?"
+      //
+      // NOTE: simeshev - 2022-10-23 - Changing the module name to something completely
+      // disconnected from the source code structure lead to this error:
+      //    "com.google.gwt.junit.JUnitFatalLaunchException: The test class
+      //    'com.effectiveem.app.table.TableRowVOTest' was not found in module
+      //    'com.some.completely.different.package.App'; no compilation unit for that type was seen"
       return "com.effectiveem.app.App";
    }
 }
